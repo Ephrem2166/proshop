6b58021a8402e8d5fc5c1157af54bf4b927c5276
Ephrem2166 <ephrem2181@gmail.com>
2023-10-10 13:39:38 +0300

    Add to Cart Functionality

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -6,7 +6,23 @@ const initialState = localStorage.getItem("cart")
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+      state.itemsPrice = state.cartItems.reduce(
+        (acc, item) => acc + item.price * item.qty,
+        0
+      );
+    },
+  },
 });
 
 export default cartSlice.reducer;
