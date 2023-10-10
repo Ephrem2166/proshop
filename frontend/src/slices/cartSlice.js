678dc202c35993b0047bde48a725e63a63e6fa5e
Ephrem2166 <ephrem2181@gmail.com>
2023-10-10 13:47:01 +0300

    Added Price Functionality to Carts

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -3,6 +3,9 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
+const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
 const cartSlice = createSlice({
   name: "cart",
   initialState,
@@ -17,12 +20,22 @@ const cartSlice = createSlice({
       } else {
         state.cartItems = [...state.cartItems, item];
       }
-      state.itemsPrice = state.cartItems.reduce(
-        (acc, item) => acc + item.price * item.qty,
-        0
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
       );
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+      state.taxPrice = addDecimals(
+        Number((0.15 * state.itemsPrice).toFixed(2))
+      );
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2);
+      localStorage.setItem("cart", JSON.stringify(state));
     },
   },
 });
 
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
