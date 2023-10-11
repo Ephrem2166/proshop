966f373b4d7ec0124292d1a42577ad494a921590
Ephrem2166 <ephrem2181@gmail.com>
2023-10-11 15:48:19 +0300

    Logout Implemented on Header

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -21,5 +21,5 @@ const authSlice = createSlice({
   },
 });
 
-export const { setCredentials } = authSlice.actions;
+export const { setCredentials, logout } = authSlice.actions;
 export default authSlice.reducer;
