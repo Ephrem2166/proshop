dd34329f9e49105b1d5baccc597c521553b587b6
Ephrem2166 <ephrem2181@gmail.com>
2023-10-11 14:26:26 +0300

    Update user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,7 +70,25 @@ const getUserProfile = asyncHandler(async (req, res) => {
 });
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send("update user profile");
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.status(200).json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 const getUsers = asyncHandler(async (req, res) => {
