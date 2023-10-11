16229481ddfb27c9a2373559314fff14408c20ba
Ephrem2166 <ephrem2181@gmail.com>
2023-10-11 13:26:46 +0300

    JSON Web Token

--- backend/data/users.js
+++ backend/data/users.js
@@ -11,13 +11,11 @@ const users = [
     name: "Ephrem Getachew",
     email: "ephrem@email.com",
     password: bcrypt.hashSync("123456", 10),
-    isAdmin: true,
   },
   {
     name: "John Doe",
     email: "john@email.com",
     password: bcrypt.hashSync("123456", 10),
-    isAdmin: true,
   },
 ];
 
