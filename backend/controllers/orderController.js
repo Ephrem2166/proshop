3f16e1bc217b3790757c32f63e5c02043c58821f
Ephrem2166 <ephrem2181@gmail.com>
2023-10-12 15:26:26 +0300

    Get Order By Id

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -40,7 +40,16 @@ const getMyOrders = asyncHandler(async (req, res) => {
 });
 
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send("Get order by ID");
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order Not Found");
+  }
 });
 
 const updateOrderToPaid = asyncHandler(async (req, res) => {
