949e0c4ca642eb9e1d76266b034ee3e694c7e265
Ephrem2166 <ephrem2181@gmail.com>
2023-10-15 13:15:42 +0300

    Product Edit Route Update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -49,10 +49,7 @@ const router = createBrowserRouter(
       <Route path="" element={<AdminRoute />}>
         <Route path="/admin/orderlist" element={<OrderListScreen />} />
         <Route path="/admin/productlist" element={<ProductListScreen />} />
-        <Route
-          path="/admin/productlist/:pageNumber"
-          element={<ProductEditScreen />}
-        />
+        <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
       </Route>
     </Route>
   )
