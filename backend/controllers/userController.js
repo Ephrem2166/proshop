851a56de0c9516dd3a85c6c0d3ca80c95b762aea
Ephrem2166 <ephrem2181@gmail.com>
2023-10-15 14:52:23 +0300

    Get Users By ID

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -97,7 +97,13 @@ const getUsers = asyncHandler(async (req, res) => {
 });
 
 const getUserById = asyncHandler(async (req, res) => {
-  res.send("get user by id");
+  const user = await User.findById(req.params.id).select("-password");
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 const deleteUser = asyncHandler(async (req, res) => {
