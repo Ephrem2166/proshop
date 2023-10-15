72d953e90fbebcc11dd7138b356ed07c2e3fa478
Ephrem2166 <ephrem2181@gmail.com>
2023-10-15 17:35:11 +0300

    Get Top Products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -106,6 +106,11 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 });
 
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+  res.status(200).json(products);
+});
+
 export {
   getProductById,
   getProducts,
@@ -113,4 +118,5 @@ export {
   updateProduct,
   deleteProduct,
   createProductReview,
+  getTopProducts,
 };
