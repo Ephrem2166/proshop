b208cf4d7bb0b1f6f9e059cfe85a56f74e38e071
Ephrem2166 <ephrem2181@gmail.com>
2023-10-15 14:55:27 +0300

    Delete User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -107,7 +107,18 @@ const getUserById = asyncHandler(async (req, res) => {
 });
 
 const deleteUser = asyncHandler(async (req, res) => {
-  res.send("Delete user");
+  const user = await User.findById(req.params.id);
+  if (user) {
+    if (user.isAdmin) {
+      res.status(400);
+      throw new Error("Can not delete admin user");
+    }
+    await User.deleteOne({ _id: user._id });
+    res.status(200).json({ message: "User deleted" });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 const updateUser = asyncHandler(async (req, res) => {
