fef7e4f0d15da9209a6976a4a70aea30cfa30738
Ephrem2166 <ephrem2181@gmail.com>
2023-10-15 17:27:09 +0300

    Search Component Added

---
+++ frontend/src/components/Search.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useParams, useNavigate } from "react-router-dom";
+
+const Search = () => {
+  const navigate = useNavigate();
+  const { keyword: URLKeyword } = useParams();
+  const [keyword, setKeyword] = useState(URLKeyword || "");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (keyword.trim()) {
+      navigate(`/search/${keyword}`);
+    } else {
+      navigate("/");
+    }
+  };
+  return (
+    <Form onSubmit={submitHandler} className="d-flex">
+      <Form.Control
+        type="text"
+        name="q"
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+        placeholder="Search Products..."
+        className="mr-sm-2 ml-sm-5"
+      ></Form.Control>
+      <Button type="submit" variant="outline-success" className="p-2 mx-2">
+        Search
+      </Button>
+    </Form>
+  );
+};
+
+export default Search;
