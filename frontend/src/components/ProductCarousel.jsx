9b0596d01a2ef5feb4523ff7883eb85af9e2e528
Ephrem2166 <ephrem2181@gmail.com>
2023-10-15 17:44:32 +0300

    Product Carousel Created

---
+++ frontend/src/components/ProductCarousel.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import { Carousel, Image } from "react-bootstrap";
+import Loader from "./Loader";
+import Message from "./Message";
+import { useGetTopProductsQuery } from "../slices/productsApiSlice";
+
+const ProductCarousel = () => {
+  const { data, isLoading, error } = useGetTopProductsQuery();
+  return isLoading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant="danger">{error?.data?.message || error.error}</Message>
+  ) : (
+    <Carousel pause="hover" className="bg-dark">
+      {data.products.map((product) => (
+        <Carousel.Item key={product._id}>
+          <Link to={`/products/${product._id}`}>
+            <Image src={product.image} alt={product.name} fluid />
+            <Carousel.Caption className="carousel-caption">
+              <h2>
+                {product.name} (${product.price})
+              </h2>
+            </Carousel.Caption>
+          </Link>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
+
+export default ProductCarousel;
