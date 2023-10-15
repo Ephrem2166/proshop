f90ea135a9f7b68e6dfa5358a61403f0a96f9f90
Ephrem2166 <ephrem2181@gmail.com>
2023-10-15 17:00:02 +0300

    Pagination Component Added to HomeScreen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -6,6 +6,7 @@ import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Loader from "../components/Loader";
 // import axios from "axios";
 import Message from "../components/Message";
+import Paginate from "../components/Paginate";
 
 const HomeScreen = () => {
   const { pageNumber } = useParams();
@@ -29,6 +30,7 @@ const HomeScreen = () => {
               </Col>
             ))}
           </Row>
+          <Paginate pages={data.pages} page={data.page} />
         </>
       )}
     </>
