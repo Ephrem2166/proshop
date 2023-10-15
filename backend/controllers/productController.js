bf87ecb2fe5ed50b6c8da5ce1e6ee39b813751f5
Ephrem2166 <ephrem2181@gmail.com>
2023-10-15 14:41:53 +0300

    Delete Product on the Server

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -53,4 +53,21 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProductById, getProducts, createProduct, updateProduct };
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await product.deleteOne({ _id: product._id });
+    res.status(200).json({ message: "Product deleted successfully" });
+  } else {
+    res.status(404);
+    throw new Error("Product Not Found");
+  }
+});
+
+export {
+  getProductById,
+  getProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
